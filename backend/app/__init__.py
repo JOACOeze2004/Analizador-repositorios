b0cd1f802a241c12fac0c419ebb1d4762f37b85d
JOACOeze2004 <jmendeza@fi.uba.ar>
2026-03-20 14:39:39 -0300

    Agrego majmanejo manual de Cors.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask import make_response
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from config import Config
 import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from config import Config
@@ -12,10 +14,21 @@ def create_app():
     app.config.from_object(Config)
  
     db.init_app(app)
-    CORS(app, resources={r"/*": {"origins": [
-    "http://localhost:8080",
-    "https://analizador-repositorios-production.up.railway.app"
-    ]}})
+    @app.after_request
+    def add_cors_headers(response):
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+        return response
+
+    @app.route('/', defaults={'path': ''}, methods=['OPTIONS'])
+    @app.route('/<path:path>', methods=['OPTIONS'])
+    def handle_options(path):
+        response = make_response()
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+        return response
 
     from app.routes.analyzer import analyzer_bp
     app.register_blueprint(analyzer_bp)
